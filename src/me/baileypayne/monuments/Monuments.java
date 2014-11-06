b912d174b1e7e6404205a5e4e7c0453835d34943
Bailey Payne <Bailpy6@aol.co.uk>
2014-11-06 08:38:38 +0000

    Set Up

--- src/me/baileypayne/monuments/Monuments.java
+++ src/me/baileypayne/monuments/Monuments.java
@@ -2,10 +2,19 @@
 
 package me.baileypayne.monuments;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  *
  * @author Bailey
  */
-public class Monuments {
+public class Monuments extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+    public void onDisable(){
+
+    }
 
 }
